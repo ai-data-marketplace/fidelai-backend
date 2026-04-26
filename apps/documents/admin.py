1e2a3ca53d69e555c8b6d4f7d9e66dd177e82086
amanuelcm27 <amanuelfirew27@gmail.com>
2026-04-26 18:34:14 +0300

    registering all models in the app

--- apps/documents/admin.py
+++ apps/documents/admin.py
@@ -1,3 +1,13 @@
+from django.apps import apps
 from django.contrib import admin
+from django.contrib.admin.sites import AlreadyRegistered
 
-# Register your models here.
+
+for model in apps.get_app_config("documents").get_models():
+	if model._meta.abstract:
+		continue
+
+	try:
+		admin.site.register(model)
+	except AlreadyRegistered:
+		continue
