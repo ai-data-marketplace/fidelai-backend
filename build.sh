d2d7d7fed6167ca71dce7860163319bcd33badad
amanuelcm27 <amanuelfirew27@gmail.com>
2026-06-01 14:13:33 +0300

    build script update

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 set -o errexit
 
+apt-get update && apt-get install -y libmagic1
 pip install -r requirements.txt
-
 python manage.py collectstatic --no-input
-
 python manage.py migrate
